80ac667ddfa502f06d5bc8e94c705a9266bf74d5
TABxSAID <62609720+TABxSAID@users.noreply.github.com>
2025-09-24 16:28:27 -0500

    Update clean_data.py

--- Assignment 1/Problem 1/src/clean_data.py
+++ Assignment 1/Problem 1/src/clean_data.py
@@ -16,27 +16,17 @@ df['Weight_kg'] = df['Weight'] * 0.45359237
 # BMI calculation
 df['BMI'] = round(df['Weight_kg'] / (df['Height_m'] ** 2), 2)
 
-# Function to assign age groups based on the ranges given
-def get_age_group(age):
-    if age < 30:
-        return '<30'
-    elif 30 <= age <= 45:
-        return '30-45'
-    elif 46 <= age <= 60:
-        return '46-60'
-    else:
-        return '>60'
-
 # Assign the age group each row
-df['AgeGroup'] = df['Age'].apply(get_age_group)
+age_bins = [0, 30, 45, 60, float('inf')]
+age_labels = ['<30', '30-45', '46-60', '>60']
+df['AgeGroup'] = pd.cut(df['Age'], bins=age_bins, labels=age_labels)
 
 # c. Categorical to numeric encoding
 # Binary for frailty
 df['Frailty_binary'] = df['Frailty'].map({'Y': 1, 'N': 0}).astype('int8')
 
 # One-hot encode age groups
-age_dummies = pd.get_dummies(df['AgeGroup'], prefix='AgeGroup', dtype=int)
-df = pd.concat([df, age_dummies], axis=1)
+df = pd.concat([df, pd.get_dummies(df['AgeGroup'], prefix='AgeGroup', dtype=int)], axis=1)
 
 # Drop unnecessary columns
 df = df.drop(['Height', 'Weight', 'Frailty', 'AgeGroup'], axis=1)
